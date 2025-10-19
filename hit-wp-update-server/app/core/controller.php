569f6bc26a4a72fc487139f47d9714f44c79edf1
Kjell Heinsen <ich@kjell-heinsen.de>
2025-10-20 00:11:03 +0200

    Update Controller Core

--- hit-wp-update-server/app/core/controller.php
+++ hit-wp-update-server/app/core/controller.php
@@ -2,12 +2,21 @@
 
 namespace hitwpupdateserver\app\core;
 
+use http\Exception;
+
 class Controller extends app{
 
     protected $_view;
 
+    protected $data;
+
     public function __construct(){
         parent::__construct();
+        http_response_code(500);
+        $this->data['state']['status'] = 'fail';
+        $this->data['state']['message'] = 'Something went wrong';
+        $this->data['state']['code'] = '500';
+
         $this->_view = new View();
     }
 
